291254365a2fdeda338d4773d902d0ddda940531
austinnasu <ajbuford@asu.edu>
2020-10-03 15:26:06 -0700

    This is the first commit.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/*
+
+Name: Austin Buford
+Class: CSE 360
+
+ */
+
+public class AddingMachine {
+
+    private int total;
+
+    public AddingMachine ()
+    {
+
+        total = 0;  // not needed - included for clarity
+
+    }
+
+    public int getTotal ()
+    {
+
+        return 0;
+
+    }
+
+    public void add (int value)
+    {
+
+
+
+    }
+
+    public void subtract (int value)
+    {
+
+
+
+    }
+
+    public String toString ()
+    {
+
+        return "";
+
+    }
+
+    public void clear()
+    {
+
+
+
+    }
+}
