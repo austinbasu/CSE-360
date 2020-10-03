691e6950a6782c912417532a255ffe146b89b152
austinnasu <ajbuford@asu.edu>
2020-10-03 15:33:06 -0700

    This is a test

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -7,6 +7,13 @@ Class: CSE 360
 
  */
 
+/*
+
+Use the normal operators for the operations (+, -).
+here is no limit on the size of the String returned.
+The format of the String should separate all operations and values with a space.
+ */
+
 public class AddingMachine {
 
     private int total;
@@ -21,6 +28,7 @@ public class AddingMachine {
     public int getTotal ()
     {
 
+        //The getTotal method should return the current total
         return 0;
 
     }
@@ -28,20 +36,22 @@ public class AddingMachine {
     public void add (int value)
     {
 
-
+        //The add method should add the parameter to the total variable
 
     }
 
     public void subtract (int value)
     {
 
-
+        //The subtract method should subtract the parameter from the total variable
 
     }
 
     public String toString ()
     {
 
+        //A history of the transactions must be kept to be returned by the toString method.
+        //The history should start from the initial 0 value to the last input.
         return "";
 
     }
@@ -49,7 +59,7 @@ public class AddingMachine {
     public void clear()
     {
 
-
+        //The clear method should clear your "memory".
 
     }
 }
